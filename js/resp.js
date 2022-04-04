f3f6e02d814012eebae9a16435c862cbf0433d37
SantoshKumarYadav <santosh93ee@gmail.com>
2022-04-04 07:29:40 -0700

    uploade the file

---
+++ js/resp.js
@@ -0,0 +1,21 @@
+burger = document.querySelector('.burger');
+navbar = document.querySelector('.navbar');
+navlist = document.querySelector('.navlist');
+rightnav = document.querySelector('.rightnav');
+
+
+burger.addEventListener('click', () => {
+    rightnav.classList.toggle('v-class-resp');
+    navlist.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+    burger.classList.toggle('active');
+})
+ document.querySelectorAll(".navlink").forEach(n =>
+     n.addEventListener("click",  () =>{
+        rightnav.classList.toggle('v-class-resp');
+    navlist.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+    burger.classList.toggle('active');
+     })
+    )
+
